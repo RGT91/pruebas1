cc3c78c4381e071b9c85abbc6ab2b18123edbefd
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2018-03-12 22:26:48 -0600

    archivos creados con los tokens e ignorados

--- Proyectos/Proyecto_1/src/main/java/lexico/AnalizadorLexico.java
+++ Proyectos/Proyecto_1/src/main/java/lexico/AnalizadorLexico.java
@@ -1,22 +1,34 @@
 package lexico;
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.Reader;
+import java.io.FileReader;
+import java.lang.ArrayIndexOutOfBoundsException;
 
 public class AnalizadorLexico {
     Flexer lexer;
+    String ourputFile;
 
     public AnalizadorLexico(String archivo){
         try {
             Reader lector = new FileReader(archivo);
             lexer = new Flexer(lector);
+            String[] dirFile = archivo.split("/");
+            ourputFile = dirFile[dirFile.length-1].split("\\.")[0];
         }
         catch(FileNotFoundException ex) {
             System.out.println(ex.getMessage() + " No se encontró el archivo;");
+        }catch (ArrayIndexOutOfBoundsException aie) {
+            System.out.println(aie.getMessage() + " No se encontró el archivo;");
         }
     }
 
     public void analiza(){
-        try{
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("out/"+ourputFile+".plx"))) {
           lexer.yylex();
+          bw.write(lexer.tokens);
         }catch(IOException ioe){
             System.out.println(ioe.getMessage());
         }
--- Proyectos/Proyecto_1/src/main/java/lexico/Test.java
+++ Proyectos/Proyecto_1/src/main/java/lexico/Test.java
@@ -4,10 +4,10 @@ import java.io.*;
 public class Test {
 
     public static void main (String[] args){
-        AnalizadorLexico al = new AnalizadorLexico("src/main/resources/fizzbuzz.py");
+        AnalizadorLexico al = new AnalizadorLexico("resources/fizzbuzz.py");
         al.analiza();
         al.tokens();
-        al = new AnalizadorLexico("src/main/resources/fz_error_lexema.py");
+        al = new AnalizadorLexico("resources/fz_error_lexema.py");
         al.analiza();
         al.tokens();
     }
