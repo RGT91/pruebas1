48d4fd83b406f767edf951153caeaa5b8220f044
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2018-03-12 20:54:50 -0600

    reconocimiento de error de lexema

--- Proyectos/Proyecto_1/src/main/java/lexico/AnalizadorLexico.java
+++ Proyectos/Proyecto_1/src/main/java/lexico/AnalizadorLexico.java
@@ -22,4 +22,8 @@ public class AnalizadorLexico {
         }
     }
 
+    public void tokens(){
+        System.out.println(lexer.tokens);
+    }
+
 }
--- Proyectos/Proyecto_1/src/main/java/lexico/Test.java
+++ Proyectos/Proyecto_1/src/main/java/lexico/Test.java
@@ -6,5 +6,9 @@ public class Test {
     public static void main (String[] args){
         AnalizadorLexico al = new AnalizadorLexico("src/main/resources/fizzbuzz.py");
         al.analiza();
+        al.tokens();
+        al = new AnalizadorLexico("src/main/resources/fz_error_lexema.py");
+        al.analiza();
+        al.tokens();
     }
 }
