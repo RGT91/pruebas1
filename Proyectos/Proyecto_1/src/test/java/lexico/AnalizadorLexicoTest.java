d2fa09f45e5eb16d3617dbd6d96c7121c49d63b4
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2018-03-14 23:01:30 -0600

    añadí JUnit al pom
    y unas pruebas dummys

---
+++ Proyectos/Proyecto_1/src/test/java/lexico/AnalizadorLexicoTest.java
@@ -0,0 +1,21 @@
+package lexico;
+
+import junit.framework.TestCase;
+
+public class AnalizadorLexicoTest extends TestCase {
+   protected void setUp() throws Exception {
+      super.setUp();
+   }
+
+   protected void tearDown() throws Exception {
+      super.tearDown();
+   }
+
+   public final void testGetSuma() {
+      assertEquals(2, 1 + 1); // TODO
+   }
+
+   public final void testIncrementa() {
+      assertEquals(2, 1 + 1); // TODO
+   }
+}
